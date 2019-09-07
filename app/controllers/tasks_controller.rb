ea4a211cc3afdbeab028952ddee215dc44e1d2a9
Kevin Wang <kevin0117@gmail.com>
2019-09-07 19:47:28 +0800

    1stPR_minor_conding_style_adjustment

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,9 @@
 class TasksController < ApplicationController
-    def index
-      @tasks = Task.all
-    end
-  
-    def new
-      @task = Task.new
-    end
-  
-
+  def index
+    @tasks = Task.all
   end
+
+  def new
+    @task = Task.new
+  end
+end
