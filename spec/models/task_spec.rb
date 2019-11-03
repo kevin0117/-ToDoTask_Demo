a681d37bbf673faae72e2dcd8b3e5fc0b7a477f3
Kevin Wang <kevin0117@gmail.com>
2019-11-03 16:36:00 +0800

    任務資訊欄位測試

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -23,9 +23,76 @@ RSpec.describe Task, type: :model do
       expect(task).to be_valid
     end
 
-    it "task title is empty" do
-      task = Task.new
-      expect(task).not_to be_valid
-    end          
+    it "title field is blank" do
+      task = Task.new(title: "", 
+                      content: "buy apple", 
+                      task_begin: "2019-11-01 09:00:00",
+                      task_end: "2019-11-01 00:00:00",
+                      priority: "urgent",
+                      status: "pending")
+        expect {
+          expect(task).to be_valid
+        }.to raise_exception(/Title can't be blank/)
+    end
+    
+    it "content field is blank" do
+      task = Task.new(title: "Shopping", 
+                      content: "", 
+                      task_begin: "2019-11-01 09:00:00",
+                      task_end: "2019-11-01 00:00:00",
+                      priority: "urgent",
+                      status: "pending")
+        expect {
+          expect(task).to be_valid
+        }.to raise_exception(/Content can't be blank/)
+    end
+    
+    it "Task begin field is blank" do
+      task = Task.new(title: "Shopping", 
+                      content: "buy apple", 
+                      task_begin: "",
+                      task_end: "2019-11-01 00:00:00",
+                      priority: "urgent",
+                      status: "pending")
+        expect {
+          expect(task).to be_valid
+        }.to raise_exception(/Task begin can't be blank/)
+    end
+    
+    it "Task end field is blank" do
+      task = Task.new(title: "Shopping", 
+                      content: "buy apple", 
+                      task_begin: "2019-11-01 09:00:00",
+                      task_end: "",
+                      priority: "urgent",
+                      status: "pending")
+        expect {
+          expect(task).to be_valid
+        }.to raise_exception(/Task end can't be blank/)
+    end
+    
+    it "priority field is blank" do
+      task = Task.new(title: "Shopping", 
+                      content: "buy apple", 
+                      task_begin: "2019-11-01 09:00:00",
+                      task_end: "2019-11-01 00:00:00",
+                      priority: "",
+                      status: "pending")
+        expect {
+          expect(task).to be_valid
+        }.to raise_exception(/Priority can't be blank/)
+    end
+    
+    it "status field is blank" do
+      task = Task.new(title: "Shopping", 
+                      content: "buy apple", 
+                      task_begin: "2019-11-01 09:00:00",
+                      task_end: "2019-11-01 00:00:00",
+                      priority: "urgent",
+                      status: "")
+        expect {
+          expect(task).to be_valid
+        }.to raise_exception(/Status can't be blank/)
+    end           
   end
 end
