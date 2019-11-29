b8fb0edb3832fbaad5cfe75a8bc928350774d87b
Kevin Wang <kevin0117@gmail.com>
2019-11-29 22:32:48 +0800

    完成基本搜尋及排序功能

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,8 +2,8 @@ class TasksController < ApplicationController
   before_action :find_task, only: [:show, :edit, :update, :destroy]
 
   def index
-    # @tasks = Task.all.order(created_at: :desc)
-    @tasks = Task.all.order(task_end: :asc)
+    @q = Task.ransack(params[:q])
+    @tasks = @q.result(distinct: true)
   end
 
   def show
