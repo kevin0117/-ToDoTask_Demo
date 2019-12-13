866c5ef6de33c52aafb7f5127ae52ab93b758276
Kevin Wang <kevin0117@gmail.com>
2019-12-13 21:36:29 +0800

    加入新增使用者、列表頁面及需要的callback functions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
+  before_action :find_user, only:[:edit,:show, :update, :destroy]
+  before_action :same_user_check, only: [:edit,:update, :destroy]
+  before_action :login_check, except: [:new]
 
   def index
     @users = User.all
+
   end
 
   def new
@@ -18,8 +22,39 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to users_path, notice: "更新帳戶成功"
+    else
+      @error_message = @user.errors.full_messages.to_sentence
+      render :edit
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      redirect_to users_path, notice: "刪除帳戶成功"
+    end
+  end
+
   private
+  def find_user
+    @user = User.find_by(id: params[:id])
+  end
+
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def same_user_check
+    if current_user != @user
+      redirect_to users_path, notice: "你只能編輯或刪除自己的帳戶資訊"
+    end
   end
 end
