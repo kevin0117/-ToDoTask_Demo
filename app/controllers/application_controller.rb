ac6876c9aa61f3ba798dc7b647f4ee066b60149e
Kevin Wang <kevin0117@gmail.com>
2019-12-13 23:37:35 +0800

    設定一些方法給登入及排序時需要

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,4 +20,38 @@ class ApplicationController < ActionController::Base
     parsed_locale = request.subdomains.first
     I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
   end
+
+  helper_method :current_user, :logged_in?, :ransack_obj, :ransack_tasks
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def login_check
+    if !logged_in?
+      redirect_to root_path, notice: "請先登入"
+    end
+  end
+
+  def same_user_check
+    if !(current_user.id == @task.user_id)
+      redirect_to users_path, notice: "你只能編輯或刪除自己的任務"
+    end
+  end
+
+  def ransack_obj
+    @q = Task.ransack(params[:q])
+  end
+
+  def ransack_tasks
+    @q = Task.ransack(params[:q])
+    @tasks = @q.result(distinct: true).page params[:page]
+  end
+  
 end
+
+
