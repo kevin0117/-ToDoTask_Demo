17d63b5ad9111deb3a24be2f0f5245bd8f6e2025
Kevin Wang <kevin0117@gmail.com>
2019-12-18 20:16:00 +0800

    管理者檢查及更新路徑

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -6,5 +6,14 @@ class Admin::BaseController < ApplicationController
     # def do_something
     #   #....
     # end
+
+    before_action :admin_check
+
+    private
+    def admin_check
+      if !current_user.admin
+        redirect_to user_tasks_path(current_user), notice: "權限不足"
+      end
+    end
     
 end
